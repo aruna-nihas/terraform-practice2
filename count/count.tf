1fab06ee1c437f04c6b2bf652aa4fea8e162dcf6
aruna <arunadegala18@gmail.com>
2024-06-29 18:50:12 +0530

    count

--- count/count.tf
+++ count/count.tf
@@ -1,16 +1,19 @@
-resource "aws_instance" "server" {
-    count = 11
-    ami = var.ami_id
-    instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql"  ? "t3.micro" : "t2.micro"
-    tags = {
-      Name = var.instance_names[count.index]
-    }
-  
+resource "aws_instance" "web" {
+  #count = 11 # count.index is a special variable given by terraform
+  count = length(var.instance_names)
+  ami           = var.ami_id #devops-practice
+  instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
+  tags = {
+    Name = var.instance_names[count.index]
+  }
 }
+
 resource "aws_route53_record" "www" {
+  #count = 11
+  count = length(var.instance_names)
   zone_id = var.zone-id
-  name    = "${var.instance_names[count.index]}.${var.domain-name}"
+  name    = "${var.instance_names[count.index]}.${var.domain-name}" #interpolation
   type    = "A"
   ttl     = 1
-  records = [var.instance_names[count.index].server == "web" ? aws_instance.sever[count.index].public_ip : aws_instance.server[count.index].private_ip]
+  records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
 }
