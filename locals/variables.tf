4599af1bd692c54e2c970535ca33ead4e314c461
aruna <arunadegala18@gmail.com>
2024-06-30 19:37:28 +0530

    functions

--- locals/variables.tf
+++ locals/variables.tf
@@ -2,15 +2,8 @@ variable "ami_id" {
     default = "ami-0b4f379183e5706b9"
   
 }
-variable "instance_names" {
-    default = ["cart","mongodb","catalogue","user","dispatch","payments","radish","mysql","rabbitmq","shipping","web"]
-  
-}
-variable "domain-name" {
-    default = "sanmdevops.online"
-  
-}
-variable "zone-id" {
-    default = "Z03699012U27NXJ39BZ50"
+variable "IsProd" {
+    type = bool
+    default = "true"
   
 }
